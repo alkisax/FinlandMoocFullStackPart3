ba17821597c6d5c64e53dda7178120a655f71d9e
Alkis <alkisax@gmail.com>
2025-02-17 11:28:48 +0200

    3.1-3.6 ok

--- index.js
+++ index.js
@@ -63,19 +63,34 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-  console.log("id created", maxId);  
-  return String(maxId + 1)
+// const generateId = () => {
+//   const maxId = persons.length > 0
+//     ? Math.max(...persons.map(n => Number(n.id)))
+//     : 0
+//   console.log("id created", maxId);  
+//   return String(maxId + 1)
+// }
+
+const generateId =() => {
+  let randomId = Math.floor(Math.random() * 10000)
+  while (persons.some(person => person.id === String(randomId))){
+    randomId = Math.floor(Math.random() * 10000)
+  }
+  return String(randomId)
 }
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  if (!body.name) {
+  if (!body.name  || !body.number) {
     return response.status(400).json({
-      error: 'name missing'
+      error: 'name/number missing'
+    })
+  }
+
+  const exists = persons.filter(person => person.name === body.name)
+  if (exists.length > 0) {
+    return response.status(400).json({
+      error: 'name must be unique' 
     })
   }
 
