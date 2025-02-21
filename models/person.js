bbbdeac6c3fe538b27e573a4a2e843ba7d87e235
Alkis <alkisax@gmail.com>
2025-02-21 17:01:15 +0200

    problems wit delete, info, post

--- models/person.js
+++ models/person.js
@@ -20,7 +20,7 @@ mongoose.connect(url)
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
       delete returnedObject._id
-      delete returnedObject._v
+      delete returnedObject.__v
     }
   })
 
